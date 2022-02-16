948ae20abf862b2dfe72ab91a3e1bf0e1d2d5797
Luka Sosiashvili <luksosiashvili@gmail.com>
2022-02-16 07:31:58 +0400

    Delete pyxcelframe directory

--- pyxcelframe/pyxcelframe/__init__.py
+++
@@ -1,4 +0,0 @@
-from pyxcelframe.pyxcelframe import insert_frame,\
-                                    copy_cell_style,\
-                                    sheet_to_sheet, \
-                                    column_last_row
